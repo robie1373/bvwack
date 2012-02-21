0c3af17add2e35a59c66da169c06a547ac6c6f31
Robie Lutsey <robie1373@gmail.com>
2012-02-20 23:31:20 -0500

    starting 0.0.2. Adding sane default directories

--- lib/bvwack/bvwack_version.rb
+++ lib/bvwack/bvwack_version.rb
@@ -1,3 +1,3 @@
 module BVWack
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
 end
--- pkg/bvwack-0.0.1/lib/bvwack/convert.rb
+++ pkg/bvwack-0.0.1/lib/bvwack/convert.rb
@@ -1,7 +1,7 @@
 require 'optparse'
 
-DEFAULT_CONVERT_BASE_DIR = "#{ENV['HOME']}"
-DEFAULT_CLEAN_BASE_DIR   = "#{ENV['HOME']}/bvwack-back"
+DEFAULT_CONVERT_BASE_DIR = "/Volumes/thundar/media/video"
+DEFAULT_CLEAN_BASE_DIR   = "/Volumes/thundar/media/converted"
 FFMPEG_OPTS              = "-acodec aac -ac 2 -ab 160k -s 1024x768 -vcodec libx264 -vpre slow -vpre iPod640 -vb 1200k -f mp4 -threads 2 -strict experimental"
 help_text                = "
 Usage: bvwack <options>
@@ -20,19 +20,12 @@ Notes: By default this will not work for you. You must change DEFAULT_CONVERT_BA
        the clean dir be a subdirectory of base dir or you'll be sad. I have set the
        FFMPEG options to use only 2 threads. This allows me to use my laptop while
        converting things. If you just want to hog through video try setting -threads 0
-       in FFMPEG_OPTS. Currently this only works on UNIX-like systems.
+       in FFMPEG_OPTS.
 
 Examples:
     bvwack -dw                             Shows proposed commands for 2 videos
     bvwack -w -b somedir/anotherdir -n 5   Converts 5 videos under somedir/anotherdir
     bvwack -c -n 10                        Moves 10 already converted mkv or avi files to /Volumes/thundar/medai/converted/[od-path]"
-
-@converted_files     = { }
-@not_converted_files = { }
-@to_convert = []
-
-puts "#{DEFAULT_CONVERT_BASE_DIR}"
-puts "#{DEFAULT_CLEAN_BASE_DIR}"
 
 options       = { }
 option_parser = OptionParser.new do |opts|
@@ -88,37 +81,26 @@ def get_files(args)
   end
 end
 
+@converted_files     = { }
+@not_converted_files = { }
 
-def get_all_files
-  if options[:base_dir]
-    base_dir = options[:base_dir]
-  else
-    base_dir = DEFAULT_CONVERT_BASE_DIR
-  end
+def get_all_files(base_dir = DEFAULT_CONVERT_BASE_DIR)
   Dir.chdir(base_dir)
   converted_files = Dir.glob(File.join("**", "*ipad.mp4"))
   converted_files.each do |i|
-    if Dir.glob(File.join("**", "*ipad.mp4")).include?("bvwack-back")
-      next
-    else
-      @converted_files[File.basename(i, ".ipad.mp4")] = i
-    end
+    @converted_files[File.basename(i, ".ipad.mp4")] = i
   end
-
   not_converted_files = Dir.glob(File.join("**", "*.{mkv,avi}"))
   not_converted_files.each do |i|
-    if Dir.glob(File.join("**", "*.{mkv,avi}")).include?("bvwack-back")
-      next
-    else
-      if File.basename(i).split(".").last == "mkv"
-        @not_converted_files[File.basename(i, ".mkv")] = i
-      elsif File.basename(i).split(".").last == "avi"
-        @not_converted_files[File.basename(i, ".avi")] = i
-      end
+    if File.basename(i).split(".").last == "mkv"
+      @not_converted_files[File.basename(i, ".mkv")] = i
+    elsif File.basename(i).split(".").last == "avi"
+      @not_converted_files[File.basename(i, ".avi")] = i
     end
   end
 end
 
+@to_convert = []
 
 def get_unconverted_files(converted_files, not_converted_files)
   (not_converted_files.keys - converted_files.keys).each do |key|
@@ -170,10 +152,15 @@ end
 
 option_parser.parse!
 puts options.inspect
+if options[:base_dir]
+  basedir = options[:base_dir]
+  get_all_files(basedir)
+else
+  get_all_files
+end
 
-
-
-
+get_unconverted_files(@converted_files, @not_converted_files)
+@to_clean = @not_converted_files.keys & @converted_files.keys
 
 if options[:num_files]
   limit = (options[:num_files] - 1).to_i
@@ -182,42 +169,25 @@ else
 end
 
 case
-  #when options[:help] == TRUE
-  #  puts help_text
   when options[:wack] == TRUE && options[:clean_up] == TRUE
     puts("Error! -w (--wack) and -c (--clean-up) cannot be used simultaneously.")
   when options[:list_converted] == TRUE
-    get_all_files
-    get_unconverted_files(@converted_files, @not_converted_files)
-    @to_clean = @not_converted_files.keys & @converted_files.keys
     list_converted
   when options[:dry_run] == TRUE && options[:clean_up] == TRUE
-    get_all_files
-    get_unconverted_files(@converted_files, @not_converted_files)
-    @to_clean = @not_converted_files.keys & @converted_files.keys
     (0..limit).each do
       dry_clean_up
     end
   when options[:clean_up] == TRUE
-    get_all_files
-    get_unconverted_files(@converted_files, @not_converted_files)
-    @to_clean = @not_converted_files.keys & @converted_files.keys
     (0..limit).each do
       puts "I would have run clean_up"
       #clean_up
     end
   when options[:dry_run] == TRUE && options[:wack] == TRUE
-    get_all_files
-    get_unconverted_files(@converted_files, @not_converted_files)
-    @to_clean = @not_converted_files.keys & @converted_files.keys
     (0..limit).each do |i|
       file = @to_convert[i]
       dry_run(file)
     end
   when options[:wack] == TRUE
-    get_all_files
-    get_unconverted_files(@converted_files, @not_converted_files)
-    @to_clean = @not_converted_files.keys & @converted_files.keys
     (0..limit).each do |i|
       file = @to_convert[i]
       puts "I would have run convert(file)"
@@ -226,3 +196,21 @@ case
   else
     puts help_text
 end
+
+=begin
+    (0..limit).each do |i|
+      file = @to_convert[i]
+      if options[:dry_run] == TRUE
+
+      else
+        if options[:clean_up] == TRUE
+          puts "I would have run clean_up"
+          #clean_up
+        else
+          puts "I would have run convert(file)"
+          #convert(file)
+        end
+      end
+    end
+end
+=end
