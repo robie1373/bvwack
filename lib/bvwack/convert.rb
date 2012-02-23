ad6727c86a97f4305ec0ea32a067d3a8ba911bdd
Robie Lutsey <robie1373@gmail.com>
2012-02-22 19:43:25 -0500

    help extracted to class

--- lib/bvwack/convert.rb
+++ lib/bvwack/convert.rb
@@ -1,38 +1,13 @@
 require 'optparse'
+require_relative 'help'
 
 DEFAULT_CONVERT_BASE_DIR = "#{ENV['PWD']}"
 DEFAULT_CLEAN_BASE_DIR   = "#{ENV['PWD']}/bvwack-back"
 FFMPEG_OPTS              = "-acodec aac -ac 2 -ab 160k -s 1024x768 -vcodec libx264 -vpre slow -vpre iPod640 -vb 1200k -f mp4 -threads 2 -strict experimental"
-help_text                = "
-Usage: bvwack <options>
-
-Default action is equivalent to bvwack -n 2
-
-Options:
-    -b BASE_DIR, --base-dir BASE_DIR  Set BASE_DIR instead of /Volumes/thundar/media/video
-    -d, --dry-run                     Do a dry run. Prints proposed commands to STDOUT.
-    -c, --clean-up                    Instead of converting, move all converted files to /Volumes/thundar/media/converted/[old-path]
-    -n #, --num-files #               Number of videos to batch.
-    -l, --list_converted              Lists files that have been converted but not cleaned. Useful for vierifying successful conversion.
-
-Notes: By default this will not work for you. You must change DEFAULT_CONVERT_BASE_DIR
-       and DEFAULT_CLEAN_BASE_DIR to something that exists on your system. Do not let
-       the clean dir be a subdirectory of base dir or you'll be sad. I have set the
-       FFMPEG options to use only 2 threads. This allows me to use my laptop while
-       converting things. If you just want to hog through video try setting -threads 0
-       in FFMPEG_OPTS. Currently this only works on UNIX-like systems.
-
-Requirements: ffmpeg and a libx264-slow.ffpreset (possibly in your ~/.ffmpeg/ directory or in your Cellar if you use brew.) Google is your friend.
-
-Examples:
-    bvwack -dw                             Shows proposed commands for 2 videos
-    bvwack -w -b somedir/anotherdir -n 5   Converts 5 videos under somedir/anotherdir
-    bvwack -c -n 10                        Moves 10 already converted mkv or avi files to /Volumes/thundar/medai/converted/[od-path]"
 
 @converted_files     = { }
 @not_converted_files = { }
 @to_convert          = []
-
 
 options       = { }
 option_parser = OptionParser.new do |opts|
@@ -244,5 +219,6 @@ case
       convert(file)
     end
   else
-    puts help_text
+    man = Help.new
+    man.be_helpful
 end
