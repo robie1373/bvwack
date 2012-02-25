9bdc3d154745ce13c312c72c717546f23ad59446
Robie Lutsey <robie1373@gmail.com>
2012-02-25 01:05:54 -0500

    Added configuratoin constant for input file types. As long as the ffmpeg command switches support other file types adding them to the INPUT_FILE_FORMATS array should cause them to be converted. No testing of other formats has been done so far.

--- lib/bvwack/constants.rb
+++ lib/bvwack/constants.rb
@@ -3,3 +3,4 @@ DEFAULT_CLEAN_BASE_DIR   = "#{ENV['PWD']}/bvwack-back"
 DEFAULT_ITERATION_LIMIT  = 2
 DEFAULT_THREADS          = 3
 FFMPEG_OPTS              = "-acodec aac -ac 2 -ab 160k -s 1024x768 -vcodec libx264 -vpre slow -vpre iPod640 -vb 1200k -f mp4 -threads #{DEFAULT_THREADS} -strict experimental"
+INPUT_FILE_FORMATS = %w{ mkv avi }
--- lib/bvwack/filelistgetter.rb
+++ lib/bvwack/filelistgetter.rb
@@ -1,62 +1,74 @@
-  class FileListGetter
-    def initialize(options)
-      @options             = options
-      @converted_files     = { }
-      @not_converted_files = { }
-      @to_convert          = []
-      get_all_files
-      #puts "filelistgetter#init to_clean_list #{to_clean}"
+class FileListGetter
+  def initialize(options)
+    @options             = options
+    @converted_files     = { }
+    @not_converted_files = { }
+    @to_convert          = []
+    get_all_files
+    #puts "filelistgetter#init to_clean_list #{to_clean}"
+  end
+
+  def get_all_files
+    if @options[:base_dir]
+      base_dir = @options[:base_dir]
+    else
+      base_dir = DEFAULT_CONVERT_BASE_DIR
+    end
+    Dir.chdir(base_dir)
+    converted_files = Dir.glob(File.join("**", "*ipad.mp4"))
+    converted_files.each do |i|
+      if i.include?("bvwack-back")
+        next
+      else
+        @converted_files[File.basename(i, ".ipad.mp4")] = i
+      end
     end
 
-    def get_all_files
-      if @options[:base_dir]
-        base_dir = @options[:base_dir]
+    puts "filelistgetter#get_all_files extension_grep_pattern #{extension_grep_pattern}"
+    not_converted_files = Dir.glob(File.join("**", "*.{#{extension_grep_pattern}}"))
+    not_converted_files.each do |i|
+      if i.include?("bvwack-back")
+        next
       else
-        base_dir = DEFAULT_CONVERT_BASE_DIR
-      end
-      Dir.chdir(base_dir)
-      converted_files = Dir.glob(File.join("**", "*ipad.mp4"))
-      converted_files.each do |i|
-        if i.include?("bvwack-back")
-          next
-        else
-          @converted_files[File.basename(i, ".ipad.mp4")] = i
-        end
-      end
-      not_converted_files = Dir.glob(File.join("**", "*.{mkv,avi}"))
-      not_converted_files.each do |i|
-        if i.include?("bvwack-back")
-          next
-        else
-          if File.basename(i).split(".").last == "mkv"
-            @not_converted_files[File.basename(i, ".mkv")] = i
-          elsif File.basename(i).split(".").last == "avi"
-            @not_converted_files[File.basename(i, ".avi")] = i
+        file_formats.each do |format|
+          if File.basename(i).split(".").last == format
+            @not_converted_files[File.basename(i, ".#{format}")] = i
+          #elsif File.basename(i).split(".").last == "avi"
+          #  @not_converted_files[File.basename(i, ".avi")] = i
           end
         end
       end
     end
-
-    def converted_files
-      @converted_files
-    end
-
-    def not_converted_files
-      @not_converted_files
-    end
-
-    def get_unconverted_files
-      (@not_converted_files.keys - @converted_files.keys).each do |key|
-        @to_convert << @not_converted_files[key]
-      end
-      @to_convert
-    end
-
-    def to_clean
-      not_converted_files.keys & converted_files.keys
-    end
-
-    def lists
-      {:converted_files => converted_files, :not_converted_files => not_converted_files, :to_convert => get_unconverted_files, :to_clean => to_clean}
-    end
   end
+
+  def file_formats
+    INPUT_FILE_FORMATS
+  end
+
+  def extension_grep_pattern
+    file_formats.join(',')
+  end
+
+  def converted_files
+    @converted_files
+  end
+
+  def not_converted_files
+    @not_converted_files
+  end
+
+  def get_unconverted_files
+    (@not_converted_files.keys - @converted_files.keys).each do |key|
+      @to_convert << @not_converted_files[key]
+    end
+    @to_convert
+  end
+
+  def to_clean
+    not_converted_files.keys & converted_files.keys
+  end
+
+  def lists
+    { :converted_files => converted_files, :not_converted_files => not_converted_files, :to_convert => get_unconverted_files, :to_clean => to_clean }
+  end
+end
