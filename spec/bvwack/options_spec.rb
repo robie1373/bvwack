c798957a56e24420bf51e707067d31f86ca80fc0
Robie Lutsey <robie1373@gmail.com>
2012-02-26 21:07:33 -0500

    tests for options.

--- spec/bvwack/options_spec.rb
+++ spec/bvwack/options_spec.rb
@@ -2,7 +2,25 @@ require "rspec"
 require_relative '../spec_helper'
 
 describe GetOptions do
+  before :each do
+    #ARGV = ['-b foo/bar', '-w']
+    #@options = GetOptions.new().put_options
+
+  end
   describe "#put_options" do
-    it "should do its thing"
+    @switches = { ['-d'] => { :dry_run => true },
+              ['-b foo/bar'] => { :base_dir => ' foo/bar' },
+              ['-c'] => { :clean_up => true },
+              ['-n5'] => { :num_files => 5 },
+              ['-h'] => { :help => true },
+              ['-l'] => { :list_converted => true},
+              ['-w'] => { :wack => true } }
+    @switches.each do |switch|
+      it "options should be #{switch[0]} " do
+        ARGV     = switch[0]
+        @options = GetOptions.new().put_options
+        @options.should == switch[1]
+      end
+    end
   end
 end
