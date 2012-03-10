1e0638fddc7622a957aa493806b3db0704cd790b
Robie Lutsey <robie1373@gmail.com>
2012-03-09 21:26:21 -0500

    cleaned up bvwack.rb

--- lib/bvwack.rb
+++ lib/bvwack.rb
@@ -1,20 +1,3 @@
-#require_relative 'bvwack/options'
 require_relative 'bvwack/actionator'
 
-#require_relative 'bvwack/lister'
-#require_relative 'bvwack/drycleaner'
-#require_relative 'bvwack/cleaner'
-#require_relative 'bvwack/echobasedirs'
-#require_relative 'bvwack/runner'
-#require_relative 'bvwack/wacker'
-#require_relative 'bvwack/drywacker'
-#require_relative 'bvwack/limiter'
-#require_relative 'bvwack/filelistgetter'
-#require_relative 'bvwack/constants'
-#require_relative 'bvwack/fileobj'
-#require_relative 'bvwack/help'
-
-
-#file_obj = FileObj.new(lists)
-#puts "bvwack lists #{lists[:to_clean]}"
 Actionator.new.actionate
